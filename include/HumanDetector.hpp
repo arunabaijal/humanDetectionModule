fbfb9e3f49251ab4828fe3adf71fc48ccbb8452a
Aruna Baijal <aruna11baijal@gmail.com>
2019-10-10 17:08:22 -0400

    Initial header file for HumanDetector Module

---
+++ include/HumanDetector.hpp
@@ -0,0 +1,50 @@
+/*@
+ *@file HumanDetector.hpp
+ *@author Aruna Baijal
+ *@brief This class controls the module that detects humans in an image vector
+ *@copyright 2019 Aruna Baijal
+ */
+#ifndef INCLUDE_HUMANDETECTOR_HPP_
+#define INCLUDE_HUMANDETECTOR_HPP_
+
+#include <opencv2/objdetect.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+
+class HumanDetector {
+ private:
+  cv::HOGDescriptor detector;
+  std::vector<cv::Rect> boundary;
+  cv::Mat imageFrame;
+
+ public:
+  /*
+   * @param image Mat representation of the image
+   * @return vector<Rect> value of boundaries detected
+   */
+  std::vector<cv::Rect> detectHuman(cv::Mat image);
+
+  /*
+   * @param boundary vector<Rect> detected boundary
+   * @return vector<Rect> optimised boundary vector
+   */
+  std::vector<cv::Rect> improveBoundary(std::vector<cv::Rect> boundary);
+
+  std::vector<cv::Rect> getBoundary() const {
+    return boundary;
+  }
+
+  void setBoundary(std::vector<cv::Rect> boundary) {
+    this->boundary = boundary;
+  }
+
+  cv::Mat getImageFrame() const {
+    return imageFrame;
+  }
+
+  void setImageFrame(cv::Mat imageFrame) {
+    this->imageFrame = imageFrame;
+  }
+};
+
+#endif  // INCLUDE_HUMANDETECTOR_HPP_
