97f472785eb6fe1a7e2fbd8dd627621351bcd890
Aruna Baijal <aruna11baijal@gmail.com>
2019-10-10 18:27:37 -0400

    Initial header file for ImageInput module with variables and method signatures

---
+++ include/ImageInput.hpp
@@ -0,0 +1,34 @@
+/*@
+ *@file ImageInput.hpp
+ *@author Aruna Baijal
+ *@brief This class controls the module that takes image as in input
+ *@copyright 2019 Aruna Baijal
+ */
+#ifndef INCLUDE_IMAGEINPUT_HPP_
+#define INCLUDE_IMAGEINPUT_HPP_
+
+#include <opencv2/objdetect.hpp>
+#include <opencv2/core/mat.hpp>
+#include <opencv2/core/types.hpp>
+
+class ImageInput {
+ private:
+  cv::Mat imageFrame;
+
+ public:
+  /*
+   * @param image Mat representation of the image
+   * @return vector<Rect> value of boundaries detected
+   */
+  cv::Mat readImage(std::String imagePath);
+
+  cv::Mat getImageFrame() const {
+    return imageFrame;
+  }
+
+  void setImageFrame(cv::Mat imageFrame) {
+    this->imageFrame = imageFrame;
+  }
+};
+
+#endif  // INCLUDE_IMAGEINPUT_HPP_
