de9282029a41fa76cc3fb2514ee413dfc01d1a91
Aruna Baijal <aruna11baijal@gmail.com>
2019-10-14 12:36:43 -0400

    [1.8] Editing source files to add stub implementation of test class HumanDetector

--- app/HumanDetector.cpp
+++ app/HumanDetector.cpp
@@ -1,5 +1,16 @@
-#include <Descriptor.hpp>
+#include <HumanDetector.hpp>
 
-std::vector<cv::Rect> Descriptor::detect(cv::Mat image) {
-  return std::vector<cv::Rect> { };
+std::vector<cv::Rect> HumanDetector::detectHuman(cv::Mat image) {
+  cv::Rect r(0, 0, 0, 0);
+  std::vector < cv::Rect > sample;
+  sample.push_back(r);
+  return sample;
+}
+
+std::vector<cv::Rect> HumanDetector::improveBoundary(
+    std::vector<cv::Rect> boundary) {
+  cv::Rect r(0, 0, 0, 0);
+  std::vector < cv::Rect > sample;
+  sample.push_back(r);
+  return sample;
 }
