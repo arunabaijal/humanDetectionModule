1685ec2b05f5d52915a4b5e4ad46077fc60b6f25
Aruna Baijal <aruna11baijal@gmail.com>
2019-10-13 21:46:31 -0400

    [1.8][HumanDetectorTest.cpp] Test cases for HumanDetector class with gmock

--- test/HumanDetectorTest.cpp
+++ test/HumanDetectorTest.cpp
@@ -11,7 +11,18 @@
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 
-TEST(humanDetector, no_human_found) {
+/*TEST(humanDetector, no_human_found) {
+  HumanDetector detector;
+  auto detectorMock = new DescriptorMock();
+  detector.setDescriptor(detectorMock);
+  cv::Rect r(446, 45, 257, 514);
+  std::vector < cv::Rect > expected;
+  expected.push_back(r);
+  cv::Mat sampleImage = cv::imread("1_Human.jpg", 1);
+  EXPECT_EQ(expected, detector.detectHuman(sampleImage));
+ }*/
+
+TEST(humanDetector, one_human_found) {
   HumanDetector detector;
   auto detectorMock = new DescriptorMock();
   detector.setDescriptor(detectorMock);
@@ -22,17 +33,21 @@ TEST(humanDetector, no_human_found) {
   EXPECT_EQ(expected, detector.detectHuman(sampleImage));
 }
 
-/*TEST(humanDetector, one_human_found) {
-  PIDController controller(0, 0, 0, 0, 0, 0);
-  EXPECT_DOUBLE_EQ(controller.compute(1, 0), 0);
-}
-
-TEST(humanDetector, two_humans_found) {
-  PIDController controller(0, 0, 0, 0, 0, 0);
-  EXPECT_DOUBLE_EQ(controller.compute(1, 0), 0);
-}
+/*TEST(humanDetector, two_humans_found) {
+  HumanDetector detector;
+  auto detectorMock = new DescriptorMock();
+  detector.setDescriptor(detectorMock);
+  cv::Rect r(446, 45, 257, 514);
+  std::vector < cv::Rect > expected;
+  expected.push_back(r);
+  cv::Mat sampleImage = cv::imread("1_Human.jpg", 1);
+  EXPECT_EQ(expected, detector.detectHuman(sampleImage));
+ }*/
 
 TEST(humanDetector, should_reduce_boundary) {
-  PIDController controller(0, 0, 0, 0, 0, 0);
-  EXPECT_DOUBLE_EQ(controller.compute(1, 0), 0);
- }*/
+  HumanDetector detector;
+  cv::Rect r1(414, 0, 321, 642);
+  cv::Rect r2(446, 45, 257, 514);
+  EXPECT_EQ(r2, detector.improve_boundary(r1));
+}
+
