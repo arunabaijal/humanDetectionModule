8304c12e48e4d2dca779dbf976adb0f1a4468d3e
umang885 <umang.rastogi96@gmail.com>
2019-10-19 23:11:19 -0400

    [ReaderWriter.cpp][2.1]Implement all methods of the class

--- app/ReaderWriter.cpp
+++ app/ReaderWriter.cpp
@@ -1,13 +1,27 @@
+/*@
+ *@file       ImageInput.cpp
+ *@author     Umang Rastogi
+ *@brief      Source file to controls the actual source implementation of reader
+ *@copyright  2019 Umang Rastogi
+ */
+
 #include <ReaderWriter.hpp>
 
-cv::Mat ReaderWriter::read(std::string imagePath) {
-  return cv::Mat();
+ReaderWriter::ReaderWriter {}
+
+cv::Mat ReaderWriter::read(std::String imagePath) {
+  // convert the image file into Mat format
+  return cv::imread(imagePath);
 }
 
 cv::Mat ReaderWriter::drawRectangle(cv::Mat image, cv::Rect boundary) {
-  return cv::Mat();
+  // update image by drawing rectangle on it
+  // refer opencv library for documentation of rectangle method
+  cv::rectangle(image, boundary.tl(), boundary.br(), cv::Scalar(255, 0, 0), 1);
+  return image;
 }
 
-void ReaderWriter::showImage(cv::Mat) {
-
+void ReaderWriter::showImage(cv::Mat image) {
+  // display image on screen
+  cv:imshow("Detector", image);
 }
