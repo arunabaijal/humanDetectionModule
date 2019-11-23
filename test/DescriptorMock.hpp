376e295e94a4fe2f459c9aa539965776c6bf68fb
Aruna Baijal <aruna11baijal@gmail.com>
2019-11-23 17:42:34 -0500

    [DescriptorMock.hpp]Changing from test double to gmock

--- test/DescriptorMock.hpp
+++ test/DescriptorMock.hpp
@@ -7,19 +7,21 @@
 #ifndef TEST_DESCRIPTORMOCK_HPP_
 #define TEST_DESCRIPTORMOCK_HPP_
 
+#include <gmock/gmock.h>
 #include <vector>
 #include <IDescriptor.hpp>
 #include <opencv2/core/mat.hpp>
 #include <opencv2/core/types.hpp>
 
-class DescriptorMock : public virtual IDescriptor {
+class DescriptorMock : public IDescriptor {
  public:
-  std::vector<cv::Rect> detect(cv::Mat image) {
+  MOCK_METHOD1(detect, std::vector<cv::Rect>(cv::Mat image));
+  /*std::vector<cv::Rect> detect(cv::Mat image) {
     cv::Rect r1(414, 0, 321, 642);
     std::vector<cv::Rect> detectedMock;
     detectedMock.push_back(r1);
     return detectedMock;
-  }
+   }*/
 };
 
 #endif  // TEST_DESCRIPTORMOCK_HPP_
